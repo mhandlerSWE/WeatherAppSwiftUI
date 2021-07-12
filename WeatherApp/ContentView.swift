6bee409684a304cbb0f13fcb34db2b2c267bf6a3
Max Handler <max.handler@rutgers.edu>
2021-07-12 14:36:12 -0400

    first commit

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("Hello, world!")
+                    .padding()
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+                }
+            }
+        }
     }
 }
 
